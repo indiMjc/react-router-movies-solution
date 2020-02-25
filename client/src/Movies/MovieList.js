a25c75a929f36f0f149a5ff18987d618c1d49cc8
Michael <indimjc1@gmail.com>
2020-02-24 21:28:47 -0500

    dry stretch goal

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import MovieCard from './MovieCard'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-const MovieList = props => {
+const MovieList = () => {
 	const [movies, setMovies] = useState([])
+
 	useEffect(() => {
 		const getMovies = () => {
 			axios
@@ -23,30 +25,8 @@ const MovieList = props => {
 		<div className='movie-list'>
 			{movies.map(movie => (
 				<Link key={movie.id} to={`/movies/${movie.id}`}>
-					<MovieDetails movie={movie} />
+					<MovieCard movie={movie} />
 				</Link>
-			))}
-		</div>
-	)
-}
-
-function MovieDetails({ movie }) {
-	const { title, director, metascore, stars } = movie
-	return (
-		<div className='movie-card'>
-			<h2>{title}</h2>
-			<div className='movie-director'>
-				Director: <em>{director}</em>
-			</div>
-			<div className='movie-metascore'>
-				Metascore: <strong>{metascore}</strong>
-			</div>
-			<h3>Actors</h3>
-
-			{stars.map(star => (
-				<div key={star} className='movie-star'>
-					{star}
-				</div>
 			))}
 		</div>
 	)
